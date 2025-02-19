a5c977fa46fe3ca2fef04ec28b018cf39237539d
akuan1997 <akuan10cc@gmail.com>
2025-02-20 01:01:13 +0800

    新增了put功能

--- routes/tasks.js
+++ routes/tasks.js
@@ -7,12 +7,13 @@ const {
   getTask,
   updateTask,
   deleteTask,
+  editTask,
 } = require("../controllers/tasks");
 
 // router.route('/').get((req, res) => {
 //     res.send('all items')
 // })
 router.route("/").get(getAllTasks).post(createTask)
-router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask)
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask).put(editTask)
 
 module.exports = router;
