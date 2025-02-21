918839ed09062459d54f8d098cb765f7c7d04ce6
akuan1997 <akuan10cc@gmail.com>
2025-02-21 09:26:46 +0800

    新增一些註腳

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -3,10 +3,10 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   // console.log(err)
   // // return res.status(500).json({msg:"something went wrong"})
   // return res.status(err.status).json({msg:err.message})
-  if (err instanceof CustomAPIError) {
+  if (err instanceof CustomAPIError) {  // 如果是自定義錯誤
     return res.status(err.statusCode).json({ msg: err.message });
   }
-  return res
+  return res // 如果不是自定義錯誤
     .status(500)
     .json({ msg: "Something went wrong, please try again." });
 };
